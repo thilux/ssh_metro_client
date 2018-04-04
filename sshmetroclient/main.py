c38bba0bf3ae383c01abc93afa72f7f9fc7bf46c
thilux <thilux.systems@gmail.com>
2018-04-04 17:22:58 -0300

    Changing main method to make it testable with arguments passed in by other python programs.

--- sshmetroclient/main.py
+++ sshmetroclient/main.py
@@ -170,11 +170,11 @@ def start_ssh_connection(username, password, host, port):
     child.interact()
 
 
-def main():
+def main(system_args):
     """
     Main application method.
     """
-    args = parser.parse_args()
+    args = parser.parse_args(system_args[1:])
 
     try:
 
@@ -195,4 +195,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
